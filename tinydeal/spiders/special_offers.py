832313621fd3037e9d62e2da4eaa5ca75744ac36
Ismail Farooq <ismaeelfarooq53@gmail.com>
2020-04-12 19:05:42 +0500

    Scraping the website special offers several pages.

--- tinydeal/spiders/special_offers.py
+++ tinydeal/spiders/special_offers.py
@@ -4,8 +4,20 @@ import scrapy
 
 class SpecialOffersSpider(scrapy.Spider):
     name = 'special_offers'
-    allowed_domains = ['www.tinydeal.com/specials.html']
-    start_urls = ['http://www.tinydeal.com/specials.html/']
+    allowed_domains = ['www.tinydeal.com']
+    start_urls = ['https://www.tinydeal.com/specials.html']
 
     def parse(self, response):
-        pass
+        products = response.xpath("//ul[@class='productlisting-ul']/div/li")
+
+        for product in products:
+            yield {
+                'title': product.xpath(".//a[@class='p_box_title']/text()").get(),
+                'url': response.urljoin(product.xpath(".//a[@class='p_box_title']/@href").get()),
+                'discounted_price': product.xpath(".//div[@class='p_box_price']/span[1]/text()").get(),
+                'orignal_price': product.xpath(".//div[@class='p_box_price']/span[2]/text()").get(),
+            }   
+
+        next_page = response.xpath(".//a[@class='nextPage']/@href").get()
+        if next_page:
+            yield scrapy.Request(url=next_page, callback=self.parse)
